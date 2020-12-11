02952d7a518021e00a76faf1e307b2db219e4142
loge moustache <jolan.pericart@gmail.com>
2020-12-11 22:17:26 +0100

    add .exe

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+#include <SFML/Graphics.hpp>
 int main()
 {
+	sf::RenderWindow generation (sf::VideoMode(1000,500),"generation");
+	while(generation.isOpen())
+	{
+		sf::Event event;
+		while (generation.pollEvent(event))
+		{
+			if (event.type==sf::Event::Closed) 
+			{
+				generation.close();
+			}
+		}
+		generation.display();
+	}
+	
 	return 0;
 }
