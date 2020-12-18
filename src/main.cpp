8b4557ce91979a1fad3cad21b294221d8fd16588
loge moustache <jolan.pericart@gmail.com>
2020-12-18 13:58:37 +0100

    the generation is broken

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,10 @@
 #include <iostream>
 int main()
 {
-	int screen_x = 1000;
-	int screen_y = 500;
+	int screen_x = 50; //valeur initial = 1000//
+	int screen_y = 500; //valeur initial = 500//
 	int min_random = 1;
 	int max_random = 100;
-	unsigned int chance_brown = 0;
 	unsigned int good_random = 20;
 	float square_size = 10.0f;
 
@@ -48,16 +47,21 @@ int main()
 
 		for (int x=0;x<screen_x/square_size;x++)
 		{
-			/*
-			sf::Color neighboring_squareG;
+			
+			sf::Color neighboring_squareL;
 			if (x > 0)
 			{
-				neighboring_squareG = map[y][x-1].getFillColor();
+				neighboring_squareL = map[y][x-1].getFillColor();
 			}
 			else
 			{
-				neighboring_squareG = pink;
+				neighboring_squareL = pink;
 			}
+			std::to_string(neighboring_squareL.r);
+			std::cout << std::to_string(neighboring_squareL.r) << std::endl;
+			std::cout << std::to_string(neighboring_squareL.g) << std::endl;
+			std::cout << std::to_string(neighboring_squareL.b) << std::endl;
+			std::cout << "" << std::endl;
 			sf::Color neighboring_squareR;
 			if (x < screen_x/square_size-1)
 			{
@@ -67,6 +71,10 @@ int main()
 			{
 				neighboring_squareR = pink;
 			}
+			std::cout << std::to_string(neighboring_squareR.r) << std::endl;
+			std::cout << std::to_string(neighboring_squareR.g) << std::endl;
+			std::cout << std::to_string(neighboring_squareR.b) << std::endl;
+			std::cout << "" << std::endl;
 			sf::Color neighboring_squareU;
 			if (y > 0)
 			{
@@ -76,6 +84,10 @@ int main()
 			{
 				neighboring_squareU = pink;
 			}
+			std::cout << std::to_string(neighboring_squareU.r) << std::endl;
+			std::cout << std::to_string(neighboring_squareU.g) << std::endl;
+			std::cout << std::to_string(neighboring_squareU.b) << std::endl;
+			std::cout << "" << std::endl;
 			sf::Color neighboring_squareD;
 			if (y < screen_y/square_size-1)
 			{
@@ -85,29 +97,43 @@ int main()
 			{
 				neighboring_squareD = pink;
 			}
-			
-			if (neighboring_squareG == brown)
+			std::cout << std::to_string(neighboring_squareD.r) << std::endl;
+			std::cout << std::to_string(neighboring_squareD.g) << std::endl;
+			std::cout << std::to_string(neighboring_squareD.b) << std::endl;
+			std::cout << "" << std::endl;
+			std::cout << "" << std::endl;
+			std::cout << "" << std::endl;
+			std::cout << "" << std::endl;
+			unsigned int chance_brown = 0;
+			if (neighboring_squareL == brown)
 			{
 				chance_brown = chance_brown + 25;
 			}
+		//	std::cout << chance_brown << std::endl;//
 			if (neighboring_squareR == brown)
 			{
 				chance_brown = chance_brown + 25;
 			}
+		//	std::cout << chance_brown << std::endl;//
 			if (neighboring_squareU == brown)
 			{
 				chance_brown = chance_brown + 25;
 			}
+		//	std::cout << chance_brown << std::endl;//
 			if (neighboring_squareD == brown)
 			{
 				chance_brown = chance_brown + 25;
 			}	
 			std::cout << chance_brown << std::endl;
+			std::cout << "" << std::endl;
+			
 			if (chance_brown < 60)
 			{
 				chance_brown = 0;
 			}
 			std::cout << chance_brown << std::endl;
+			std::cout << "" << std::endl;
+			
 			if (size_random(random) <= chance_brown)
 			{
 				map[y][x].setFillColor(brown);
@@ -116,7 +142,7 @@ int main()
 			{
 				map[y][x].setFillColor(blue);
 			}
-			*/
+			
 		}
 	}
 	for (int y=0;y<screen_y/square_size;y++)
